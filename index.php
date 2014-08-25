574cbd55bd30e6e962b26f45ecf9e6e3f9936f36
Ian Hirschfeld <ian@mightybell.com>
2014-08-24 20:35:25 -0700

    moves site out of middleman, makes it a static php site

---
+++ index.php
@@ -0,0 +1,67 @@
+
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset='utf-8'>
+    <meta content='IE=edge,chrome=1' http-equiv='X-UA-Compatible'>
+    <meta content='width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no' name='viewport'>
+    <meta content="We're a company of storytellers." name='description'>
+
+    <meta content='The Soap Collective' property='og:title'>
+    <meta content='The Soap Collective' property='og:site_name'>
+    <meta content='http://www.thesoapcollective.com' property='og:url'>
+    <meta content="We're a company of storytellers. Following the tradition that began with spoken word and evolved into books, film, and games, Soap is building new ways for people to share narrative experiences. We believe in leveraging the latest technology to imagine better worlds and connect people in meaningful ways." property='og:description'>
+    <meta content='http://www.thesoapcollective.com/images/soap_open_graph.jpg' property='og:image'>
+    <meta content='158186024916' property='fb:app_id'>
+
+    <link href='favicon.ico' type='image/x-icon' rel='shortcut icon'>
+    <link href='favicon.ico' type='image/x-icon' rel='icon'>
+
+    <title> The Soap Collective</title>
+
+    <link href='stylesheets/normailze.css' rel='stylesheet' type='text/css' />
+    <link href='stylesheets/main.css' rel='stylesheet' type='text/css' />
+  </head>
+
+  <body class='index'>
+    <div id='page-container'>
+      <header id='page-header'>
+        <h1>The Soap Collective</h1>
+      </header>
+      <article id='page-content'>
+        <p id='company-intro'>We're a company of storytellers. Following the tradition that began with spoken word and evolved into books, film, and games, Soap is building new ways for people to share narrative experiences. We believe in leveraging the latest technology to imagine better worlds and connect people in meaningful ways.</p>
+        <h2>Who are we?</h2>
+        <div id='bios'>
+          <a class='bio-container' href='https://twitter.com/ianhirschfeld' target='_blank' title="Go to Ian's Twitter page">
+            <div class='bio-image'></div>
+            <div class='bio-content'>
+              <h3>Ian Hirschfeld</h3>
+              <p>A builder and obsessive filmmaker who considers himself both engineer and artist. Invested in both telling the story and knowing how to best bring it to life.</p>
+            </div>
+          </a>
+          <a class='bio-container' href='https://twitter.com/logandwight' target='_blank' title="Go to Logan's Twitter page">
+            <div class='bio-image'></div>
+            <div class='bio-content'>
+              <h3>Logan Dwight</h3>
+              <p>Experience designer, writer, and visual artist. Searching for the next narrative leap, beyond the interaction of video games and the synesthesia of film.</p>
+            </div>
+          </a>
+        </div>
+      </article>
+    </div>
+
+    <script src='//use.typekit.net/dhn8nfe.js' type='text/javascript'></script>
+    <script>
+      // Typekit
+      try{Typekit.load();}catch(e){}
+
+      // Google Analytics
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+      ga('create', 'UA-11359166-1', 'auto');
+      ga('send', 'pageview');
+    </script>
+  </body>
+</html>
